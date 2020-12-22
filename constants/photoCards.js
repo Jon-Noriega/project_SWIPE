3eb9adff737d696f1a0a3711c7ab1f357661992b
Jon Noriega <Noriega331@gmail.com>
2020-12-22 08:38:12 -0500

    Backend initialized

--- constants/photoCards.js
+++ constants/photoCards.js
@@ -3,25 +3,29 @@ const photoCards = [
         name: "Johnny Ocean",
         photo: require("../assets/jon-noriega.jpg"),
         description: "Builder & Connector | Providence, RI",
-        project: "freeAGENT"
+        project: "freeAGENT",
+        userType: "Candidate"
     },
     {
         name: "June Bug",
         photo: require("../assets/jon-noriega.jpg"),
         description: "Enthusiastic Software Engineer",
-        project: "Staycation Station"
+        project: "Staycation Station",
+        userType: "Candidate"
     },
     {
         name: "Kelso",
         photo: require("../assets/jon-noriega.jpg"),
         description: "Enthusiastic Bad Ass",
-        project: "PlantMom"
+        project: "PlantMom",
+        userType: "Candidate"
     },
     {
         name: "Miles",
         photo: require("../assets/jon-noriega.jpg"),
         description: "Enthusiastic Baker",
-        project: "Lofty Bakes"
+        project: "Lofty Bakes",
+        userType: "Candidate"
     }
 ]
 
